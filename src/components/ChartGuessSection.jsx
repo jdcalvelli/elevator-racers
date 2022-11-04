1fb7bdf6a54d694a36930571df6004a5e5f02da9
JD Calvelli <jdcalvelli@gmail.com>
2022-11-04 13:50:45 -0400

    adding chartjs, creating first pass charts with dummy data

--- src/components/ChartGuessSection.jsx
+++ src/components/ChartGuessSection.jsx
@@ -1,9 +1,25 @@
 import {Paper} from "@mantine/core";
 
+//import chartjs core
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+import {Bar} from "react-chartjs-2";
+
 function ChartGuessSection() {
+
     return (
         <Paper shadow={"xl"} p={"md"}>
-            <p> this is where the chart js guess section will go </p>
+            <Bar
+                data={{
+                    labels: ['elevator 1', 'elevator 2', 'elevator 3'],
+                    datasets:[{
+                        label: 'guesses per elevator',
+                        data: [3, 4, 1],
+                    }]
+                }}
+                height={400}
+                width={400}/>
         </Paper>
     )
 }
